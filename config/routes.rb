a1e85df32993d5599ca565a88ffd6d50615010d4
anri-kbt <anrseys2@gmail.com>
2022-05-12 05:21:46 +0000

    first calendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   devise_for :admins, skip:[:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
@@ -7,23 +7,24 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   namespace :admin do
     resources :customers ,only:[:index,:edit,:show,:update,:destroy]
     resource :cosmetics ,only:[:destroy]
     resource :cosme_comments ,only:[:destroy]
   end
-  scope module: :public do 
+  scope module: :public do
     resources :customers ,only:[:index,:show,:edit,:update]
     get 'customers/withdrawal' => 'customers#withdrawal'
     patch 'customers/out/:id' => 'customers#out',as: :out
-    
+
     root :to =>"homes#top"
     get 'homes/about'
     resources :cosmetics
     resource :categories ,only:[:index]
     resource :brands ,only:[:index]
+    resources :calendars
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
