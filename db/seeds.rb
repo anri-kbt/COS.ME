c4ca795ef88c0af626ccdb97df1a23cae37f8238
anri-kbt <anrseys2@gmail.com>
2022-05-29 11:07:58 +0000

    Fix admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,44 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.update(
+Admin.create!(
   email: 'admin@admin',
   password: 'cos.me'
   )
 
-Category.create!(
-  category_name: "化粧下地",
- )
-Category.create!(
-  category_name: 'ファンデーション',
- )
-Category.create!(
-  category_name: 'フェイスパウダー',
- )
-Category.create!(
-  category_name: 'コンシーラー',
- )
-Category.create!(
-  category_name: 'アイブロウ',
- )
-Category.create!(
-  category_name: 'アイライナー',
- )
-Category.create!(
-  category_name: 'アイシャドウ',
- )
-Category.create!(
-  category_name: 'マスカラ',
- )
-Category.create!(
-  category_name: 'チーク',
- )
-Category.create!(
-  category_name: 'リップ',
- )
-Category.create!(
-  category_name: 'メイク小物',
- )
-Category.create!(
-  category_name: 'メイクその他',
- )
